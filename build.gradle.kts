258be2771fdce28b1405171a89d40491873d9e2e
Atto9752 <anto.faundes@duocuc.cl>
2025-11-06 16:00:25 -0300

    A

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation "androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2"
 }
